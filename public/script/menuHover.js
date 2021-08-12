300fb761935b38606ddda130d7fa6fc2ac6aacd7
mk-Yayo <amunozbaez669@gmail.com>
2021-08-12 11:54:56 -0500

    'v2.22'

--- public/script/menuHover.js
+++ public/script/menuHover.js
@@ -4,24 +4,27 @@ const bars = document.getElementById('navButton');
 const ulNav = document.querySelector('.navList');
 const menuNav = document.querySelector('.navListMenu');
 const close = document.querySelector('.fa-times');
-const menuClick = document.querySelector('.navMenu')
+const menuClick = document.querySelector('.navMenu');
+const home = document.getElementById('home');
 const locationNav = document.getElementById('locationNav');
 let menuOpen = 0;
 
 bars.addEventListener('click', e => {
-    ulNav.classList.toggle('open')
+    ulNav.classList.toggle('open');
     ulNav.style.display = 'flex';
     if (ulNav.classList.contains('close')){
-        ulNav.classList.toggle('close')
+        ulNav.classList.toggle('close');
     }
 })
 close.addEventListener('click', e => {
-    //Se resuelve error Menu Abierto
+    //Se resuelve errores al cerrar Nav
     if (menuOpen == 1){
         setTimeout(function (){
             locationNav.classList.remove('locationNav');
             menuNav.style.display = 'none';
-        }, 1000)
+            home.classList.toggle('actualPage');
+            menuClick.classList.toggle('actualPage');
+        }, 1000);
         menuOpen = 0;
     }
     if (ulNav.classList.contains('open')){
@@ -29,28 +32,32 @@ close.addEventListener('click', e => {
     }
     ulNav.classList.toggle('open');
     setTimeout(function (){
-        ulNav.style.display = 'none'
+        ulNav.style.display = 'none';
     }, 1000 );
 })
 
 menuClick.addEventListener('click', e => {
     if (menuOpen == 0){
+        home.classList.toggle('actualPage');
+        menuClick.classList.toggle('actualPage');
         menuNav.classList.remove('menuListClose');
         locationNav.classList.toggle('locationNav');
         setTimeout(function (){
-            menuNav.style.display = 'block'
-            menuNav.classList.add('menuList')
+            menuNav.style.display = 'block';
+            menuNav.classList.add('menuList');
         }, 500);
         menuOpen = 1;
     }
     else if (menuOpen == 1){
+        home.classList.toggle('actualPage');
+        menuClick.classList.toggle('actualPage');
         menuNav.classList.remove('menuList');
-        menuNav.classList.add('menuListClose')
+        menuNav.classList.add('menuListClose');
         setTimeout(function (){
-            locationNav.classList.remove('locationNav')
+            locationNav.classList.remove('locationNav');
             locationNav.classList.add('locationNavClose');
             menuNav.style.display = 'none';
-        }, 500)
+        }, 500);
         menuOpen = 0;
     }
     locationNav.classList.remove('locationNavClose');
@@ -66,24 +73,24 @@ let hoverValue = false;
 menuLink.addEventListener('mouseover', e => {
     mainPage.classList.toggle('actualPage');
         menuHover.style.display = 'flex';
-        menuLink.style.color = '#348AC0'
+        menuLink.style.color = '#348AC0';
         hoverValue = true;
     
 })
 menuHover.addEventListener('mouseover', e => {
     mainPage.classList.toggle('actualPage');
         menuHover.style.display = 'flex';
-        menuLink.style.color = '#348AC0'
+        menuLink.style.color = '#348AC0';
         hoverValue = true;
     
 })
 menuLink.addEventListener('mouseout', e => {
     mainPage.classList.toggle('actualPage');
     menuHover.style.display = 'none';
-    menuLink.style.color = '#C9C7E3'
+    menuLink.style.color = '#C9C7E3';
 })
 menuHover.addEventListener('mouseout', e => {
     mainPage.classList.toggle('actualPage');
     menuHover.style.display = 'none';
-    menuLink.style.color = '#C9C7E3'
+    menuLink.style.color = '#C9C7E3';
 })
