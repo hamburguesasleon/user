dabf9550a87d5e7bb24e8f500f07f6a6fbd7a85a
mk-Yayo <amunozbaez669@gmail.com>
2021-08-18 21:50:57 -0500

    v2.6

--- public/script/hamburguer.js
+++ public/script/hamburguer.js
@@ -1,13 +1,13 @@
 //Hamburguesas
 
 const arrayHam = [
-    ['Godinez', 'Carne de Res, Jitomate, Lechuga, Cebolla Caramelizada, Queso Manchego<br><span>¡SIN papas a la Francesa!</span>', '40', "../../img/ham.jpg", '0'],
-    ['Clásica', 'Carne de Res, Jitomate, Lechuga, Cebolla Caramelizada, Jamón, Queso Amarillo, Queso Manchego, Papas a la Francesa', '50', "../../img/classic.jpg", '1'],
-    ['Hawaiana', 'Carne de Res, Jitomate, Lechuga, Cebolla Caramelizada, Piña, Jamón, Queso Amarillo, Queso de Hebra, Papas a la Francesa', '58', '../../img/ham.jpg', '2'],
-    ['Porcina', 'Carne de Res, Jitomate, Lechuga, Cebolla Caramelizada, Chistorra, Jamón, Queso de Hebra, Queso Manchego, Papas a la Francesa', '59', '../../img/ham.jpg', '3'],
-    ['BBQ Clásica', 'Carne de Res, Aros de Cebolla, Tocino, Queso Amarillo, Queso Manchego, Salsa BBQ, Papas a la Francesa', '65', '../../img/bbq.jpg', '4'],
-    ['De León', 'Carne de Res, Jitomate, Lechuga, Aros de Cebolla, Tocino, Jamón, Queso Amarillo, Queso Manchego, Aguacate, Papas a la Francesa', '68', "../../img/hamLeon.jpg", '5'],
-    ['Rascacielos', 'Carne de Res, Jitomate, Lechuga, Aros de Cebolla, Chistorra, Salchicha, Tocino, Queso De Hebra, Queso Manchego, Aguacate, Papas a la Francesa', '78', '../../img/skyline.jpg', '6']
+    ['Godinez', 'Carne de Res, Jitomate, Lechuga, Cebolla Caramelizada, Queso Manchego<br><span>¡SIN papas a la Francesa!</span>', '40', "../../img/ham.jpg", '0', 'Lorem ipsum'],
+    ['Clásica', 'Carne de Res, Jitomate, Lechuga, Cebolla Caramelizada, Jamón, Queso Amarillo, Queso Manchego, Papas a la Francesa', '50', "../../img/classic.jpg", '1', 'Lorem ipsum'],
+    ['Hawaiana', 'Carne de Res, Jitomate, Lechuga, Cebolla Caramelizada, Piña, Jamón, Queso Amarillo, Queso de Hebra, Papas a la Francesa', '58', '../../img/ham.jpg', '2', 'Lorem ipsum'],
+    ['Porcina', 'Carne de Res, Jitomate, Lechuga, Cebolla Caramelizada, Chistorra, Jamón, Queso de Hebra, Queso Manchego, Papas a la Francesa', '59', '../../img/ham.jpg', '3', 'Lorem ipsum'],
+    ['BBQ Clásica', 'Carne de Res, Aros de Cebolla, Tocino, Queso Amarillo, Queso Manchego, Salsa BBQ, Papas a la Francesa', '65', '../../img/bbq.jpg', '4', 'Lorem ipsum'],
+    ['De León', 'Carne de Res, Jitomate, Lechuga, Aros de Cebolla, Tocino, Jamón, Queso Amarillo, Queso Manchego, Aguacate, Papas a la Francesa', '68', "../../img/hamLeon.jpg", '5', 'Lorem ipsum'],
+    ['Rascacielos', 'Carne de Res, Jitomate, Lechuga, Aros de Cebolla, Chistorra, Salchicha, Tocino, Queso De Hebra, Queso Manchego, Aguacate, Papas a la Francesa', '78', '../../img/skyline.jpg', '6', 'Lorem ipsum']
 ]
 
 //Tarjetas-Hamburguesas
@@ -32,7 +32,7 @@ arrayHam.forEach( () => {
         containerTemplate.querySelector('.hamPrice').classList.remove('sky');
     }
     containerTemplate.getElementById('hamImg').setAttribute('src', arrayHam[count][3]);
-    containerTemplate.querySelector('.ingr').innerHTML = arrayHam[count][1];
+    containerTemplate.querySelector('.ingr').innerHTML = arrayHam[count][5];
     containerTemplate.querySelector('.hamPrice i p').textContent = arrayHam[count][2];
     containerTemplate.querySelector('.menu').setAttribute('value', `ham${count}`)
     containerTemplate.querySelector('.plusHam').setAttribute('value', count)
@@ -50,7 +50,10 @@ const hamInfoTitle = document.querySelector('.hamInfoTitle');
 const hamInfoIngr = document.querySelector('.hamInfoIngr');
 const closeCard = document.querySelector('.closeCard');
 const priceInCard = document.querySelector('.hamInfoPrice');
-const hamInfoImg = document.querySelector('.hamInfoImg')
+const hamInfoImg = document.querySelector('.hamInfoImg');
+const hamInfoType = document.querySelector('.hamInfoType');
+
+const type = 'Hamburguesa'
 
 containerHam.addEventListener('click', e => {
     const plus = e.target;
@@ -68,6 +71,7 @@ containerHam.addEventListener('click', e => {
         hamInfoImg.setAttribute('src', arrayHam[select][3])
         hamInfoTitle.textContent = arrayHam[select][0];
         hamInfoIngr.innerHTML = arrayHam[select][1];
+        hamInfoType.textContent = type;
         priceInCard.textContent = arrayHam[select][2];
     }
 })
